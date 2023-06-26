a0b815631a104ee77149d8d179e45ac1e7386833
gtamaral <rodrigoamaral003@gmail.com>
2023-06-26 12:53:22 -0300

    correcao arquivo vercel e var dbURL

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@
 const mongoose = require("mongoose");
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
+const dbURL = process.env.MONGO_URL
 
 const conn = async () => {
     try {
 
-        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.bl4kdxh.mongodb.net/?retryWrites=true&w=majority`, {
+        const dbConn = await mongoose.connect(dbURL, {
             connectTimeoutMS: 300000,
         });
             
